a986eaaef82a021aa99520f117fa62e1ac0fc497
Kaiana Miguel <kaiana.miguel@gmail.com>
2022-10-24 16:25:58 -0300

    incluindo cadastro de estoque/alterando função calcularCustoUnitario

--- src/MainKaiana.java
+++ src/MainKaiana.java
@@ -8,14 +8,15 @@ import java.time.format.DateTimeFormatter;
 public class MainKaiana {
     public static void main(String[] args) {
         cadastrarProduto();
-        cadastrarServiço();
+        //cadastrarServico();
+
     }
 
     public static void cadastrarProduto() {
 
         Marca nowMarca = new Marca (
                 1,
-                JOptionPane.showInputDialog(null,"Descrição: ", "Cadastro da Marca", JOptionPane.QUESTION_MESSAGE)
+                JOptionPane.showInputDialog(null,"Marca: ", "Cadastro de Produto", JOptionPane.QUESTION_MESSAGE)
         );
 
         Produto nowProduto = new Produto (
@@ -24,9 +25,22 @@ public class MainKaiana {
                 JOptionPane.showInputDialog(null,"Tipo: ", "Cadastro de Produto", JOptionPane.QUESTION_MESSAGE),
                 nowMarca
         );
+
+
+        //rever se estoque realmente vai ficar dentro do cadastrarProduto
+        BigDecimal custoTot;
+        int quantidade;
+        BigDecimal custoUnit;
+        Estoque nowEstoque = new Estoque (
+                1,
+                nowProduto,
+                quantidade = Integer.parseInt(JOptionPane.showInputDialog(null,"Quantidade: ", "Cadastro de Produto", JOptionPane.QUESTION_MESSAGE)), //verificar como vamos controlar a quantidade no estoque
+                custoTot = new BigDecimal(JOptionPane.showInputDialog(null,"Valor Total: ", "Cadastro de Produto", JOptionPane.QUESTION_MESSAGE)),
+                custoUnit = custoTot.divide(BigDecimal.valueOf(quantidade)) //não consegui puxar a função da classe estoque
+        );
     }
 
-    public static void cadastrarServiço() {
+    public static void cadastrarServico() {
         BigDecimal valor;
         Servico nowServico = new Servico (
                 1,
