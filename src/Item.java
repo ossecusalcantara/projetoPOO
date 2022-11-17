e39b55ed05f47ecd5266030c861d377f3637d1e9
alisson-rp <alisson.rabelo.p@hotmail.com>
2022-11-17 12:41:49 -0300

    17-11-20: Ciado DAO de algumas classes

--- src/Item.java
+++ src/Item.java
@@ -1,61 +1,73 @@
 package src;
 import java.math.BigDecimal;
+import java.util.Collection;
 
-public class Item {
+public abstract class Item {
     private Integer id;
     private Integer quantidade;
+
+    private String descricao;
     private BigDecimal valorUnitario;
     private BigDecimal valorTotal;
     private BigDecimal desconto; //desconto total
     private TipoItem tipoItem;
 
-    public Item(Integer id, Integer quantidade, BigDecimal valorUnitario, BigDecimal valorTotal, BigDecimal desconto, TipoItem tipoItem) {
+    public Item(Integer id, Integer quantidade, String descricao, BigDecimal valorUnitario, BigDecimal valorTotal, BigDecimal desconto, TipoItem tipoItem) {
         this.id = id;
         this.quantidade = quantidade;
+        this.descricao = descricao;
         this.valorUnitario = valorUnitario;
         this.valorTotal = valorTotal;
         this.desconto = desconto;
         this.tipoItem = tipoItem;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setQuantidade(Integer quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public void setValorUnitario(BigDecimal valorUnitario) {
-        this.valorUnitario = valorUnitario;
-    }
-
-    public void setValorTotal(BigDecimal valorTotal) {
-        this.valorTotal = valorTotal;
-    }
-
-    public void setDesconto(BigDecimal desconto) {
-        this.desconto = desconto;
-    }
-
     public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public Integer getQuantidade() {
         return quantidade;
     }
 
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
     public BigDecimal getValorUnitario() {
         return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
     }
 
     public BigDecimal getDesconto() {
         return desconto;
     }
 
-    public BigDecimal getValorTotal() {
-        return valorTotal;
+    public void setDesconto(BigDecimal desconto) {
+        this.desconto = desconto;
     }
 
     public TipoItem getTipoItem() {
@@ -70,6 +82,5 @@ public class Item {
         valorTotal = (this.valorUnitario.multiply(BigDecimal.valueOf(quantidade)).subtract(desconto)) ;
         return valorTotal;
     }
- /// fazendo teste para git
 
 }
